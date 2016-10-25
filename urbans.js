085a43e0e361f40dd05d6abc4472325375b79fe0
TinaVasihar <tina.v53@wipro.com>
2016-10-25 12:24:33 +0530

    Add files via upload

--- urbans.js
+++ urbans.js
@@ -1,42 +1,52 @@
-var read=require('fs');
+/*creating a interface to read the file*/
+var read=require('fs');              
 var lineReader = require('readline').createInterface({
-input: read.createReadStream('urb.csv'),
+	input: read.createReadStream('urb.csv'),
 });
 
-var myjson=[];//Json object
-var heading= [];//array
-var i=0;
+/*insializing variables*/
+var jsonArray=[];           
+var heading= [];
+var row=0;
 
-lineReader.on('line', function (line) 
-{
-  
-  if(i===0){
-     heading =line.split(',');
-      i++;
-  }
-  else
-  {
-      var currentLineData = line.split(',');
-var obj={};
-      for (var j=0; j<heading.length; j++) {
-                     
-          if(j === 3){
-             myjson[heading[j]] = currentLineData[j]; 
-          }
-          if (j === 6) {
-              obj[heading[j]] = currentLineData[j]; 
-           }
-}
-		  myjson.push(obj);
-      }
-     
-      
-  
+/*Reading the data from the csv file*/
+lineReader.on('line', function (line)  {
+   if(row === 0){
+		heading = line.split(',');
+		row++;
+   }
+   else {
+	   var jsonObj = {};
+       var currentLineData = line.split(',');
+       for (var j=0; j<heading.length; j++) {
+			if(heading[j] == "AreaName"){
+				if(currentLineData[j+1]=="Total" && currentLineData[j+2]=="All ages") {
+					jsonObj[heading[j]] = currentLineData[j]; 
+					count=1;
+				}
+				else
+					count=0;
+			}
+			else if(heading[j] == "TotalPersons") {
+				if(currentLineData[j-2]=="Total" && currentLineData[j-1]=="All ages") {
+					jsonObj[heading[j]] = currentLineData[j]; 
+					count=1;
+				}
+				else
+					count=0;
+			}
+		}
+		if(count == 1)
+			jsonArray.push(jsonObj);
+              
+    }
 
 });
-lineReader.on('close',function()
-{
-	var jso=JSON.stringify(myjson);
-      read.appendFile('myoutput.json',jso,function(err) {});
 
+/*Appending the data into output file*/
+lineReader.on('close',function() {
+	var jso=JSON.stringify(jsonArray);
+    read.appendFile('hey.json',jso,function(err) {});
 });
+
+
