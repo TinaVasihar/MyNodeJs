9efb0531171319389448e001eac34c6199739ee8
TinaVasihar <tina.v53@wipro.com>
2016-10-26 17:31:03 +0530

    Final Nodejs Program

--- urbans1.js
+++ urbans1.js
@@ -1,42 +1,176 @@
-var read=require('fs');
-var lineReader = require('readline').createInterface({
-input: read.createReadStream('urb.csv'),
+var readline = require('readline');
+var fs = require('fs');
+var path=require("path");
+i=0;
+header=[];
+var json = [];
+var rl = readline.createInterface({
+input: fs.createReadStream('data.csv')
 });
 
-var myjson=[];
-var heading= [];
-var i=0;
-lineReader.on('line', function (line) 
-{
-  
-  if(i===0){
-     heading =line.split(',');
-      i++;
+rl.on('line',function(line){
+    i++;
+    if(i==1)
+    {
+      header=line.split(",");
+    }
+    else
+    {
+      var tempArray=[];
+      row = line.split(",");
+      if(row[4]=="Total")
+       {
+         for(var i = 0; i < header.length; i++)
+           if(i==5)
+          tempArray.push(row[i]);
+        else if(i==12)
+           tempArray.push(parseInt(row[i]));
+       // Add object to list
+
+           if(json.length!=0)
+           {
+               var check=0;
+               var index=-1;
+               for(var j=0;j<json.length;j++)
+               {
+                   if(json[j].includes(tempArray[0])==true)
+                   {
+                       check=1;
+                       index=j;
+                       break;
+                   }
+               }
+               if(check==1)
+                   json[index][1]=json[index][1]+tempArray[1];
+               else
+                 json.push(tempArray);
+           }
+           else
+           json.push(tempArray);
+    }
   }
-  else
-  {
-	  
-      var currentLineData = line.split(',');
-var obj={};
-	  
-      for (var j=0; j<heading.length; j++) {
-                     
-         
-          if (heading[j]=="Area Name" ||heading[j]=="Age-group" ||heading[j]=="Illiterate - Persons") 
+    });
+rl.on("close",function()
 {
-              obj[heading[j]] = currentLineData[j]; 
-          }
+
+var rl1 = readline.createInterface({
+input: fs.createReadStream('data.csv')
+});
+
+rl1.on('line',function(line)
+{
+i++;
+if(i==1)
+{
+  header=line.split(",");
 }
-		  myjson.push(obj);
-      }
-     
-      
-  
+else
+{
+      var tempArray=[];
+       row = line.split(",");
+       if(row[4]=="Total")
+       {
+         for(var i = 0; i < header.length; i++)
+        if(i==5)
+          tempArray.push(row[i]);
+        else if(i==12)
+           tempArray.push(parseInt(row[i]));
+       // Add object to list
+
+           if(json.length!=0){
+               var check=0;
+               var index=-1;
+               for(var j=0;j<json.length;j++)
+               {
+                   if(json[j].includes(tempArray[0])==true)
+                   {
+                       check=1;
+                       index=j;
+                       break;
+                   }
+               }
+               if(check==1)
+                   json[index][1]=json[index][1]+tempArray[1];
+               else
+                 json.push(tempArray);
+           }
+           else
+           json.push(tempArray);
+    }
+
+}
 
 });
-lineReader.on('close',function()
+rl1.on("close",function()
 {
-	var jso=JSON.stringify(myjson);
-      read.appendFile('myop3.json',jso,function(err) {});
 
+
+var rl2 = readline.createInterface({
+input: fs.createReadStream('data.csv')
+});
+
+rl2.on('line',function(line){
+i++;
+if(i==1)
+{
+  header=line.split(",");
+}
+else
+{
+      var tempArray=[];
+       row = line.split(",");
+       if(row[4]=="Total")
+       {
+         for(var i = 0; i < header.length; i++)
+        if(i==5)
+          tempArray.push(row[i]);
+        else if(i==12)
+           tempArray.push(parseInt(row[i]));
+       // Add object to list
+
+           if(json.length!=0)
+           {
+               var check=0;
+               var index=-1;
+               for(var j=0;j<json.length;j++)
+               {
+                   if(json[j].includes(tempArray[0])==true)
+                   {
+                       check=1;
+                       index=j;
+                       break;
+                   }
+               }
+               if(check==1)
+                   json[index][1]=json[index][1]+tempArray[1];
+               else
+                 json.push(tempArray);
+           }
+           else
+           json.push(tempArray);
+    }
+  }
+
+});
+
+rl2.on("close",function()
+{
+ var finalJson=[];
+for(var i=0;i<json.length;i++)
+{
+   var tmp={};
+   tmp["AgeGroup"]=json[i][0];
+   tmp["LiteratePerson"]=json[i][1];
+   finalJson.push(tmp);
+}
+
+//Output file path
+var outPath = path.join(__dirname, 'json2.json');
+// Convert object to string, write json to file
+fs.writeFileSync(outPath, JSON.stringify(finalJson), 'utf8',
+   function(err){console.log(err);});
+
+
+});
+});
 });
